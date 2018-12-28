49062900af17e186513a64afa042a7bdd4e776c1
karasawa-yosuke <karasawa-yosuke@dmm.com>
2018-12-28 15:38:43 +0900

    Refactoring

--- src/enter.js
+++ src/enter.js
@@ -4,23 +4,13 @@ const launchChrome = require('@serverless-chrome/lambda');
 const CDP = require('chrome-remote-interface');
 const puppeteer = require('puppeteer');
 let AWS = require('aws-sdk');
-AWS.config.update({region: 'ap-northeast-1'});
+AWS.config.update({ region: 'ap-northeast-1' });
 /** @type {SSM} */
 const ssm = new AWS.SSM();
+/** @type {SNS} */
+const sns = new AWS.SNS();
 
 module.exports.main = async (event, context, callback) => {
-    console.log(context);
-    // const req = JSON.parse(event.body);
-    // if (req.TOKEN != process.env.TOKEN) {
-    //     return {
-    //         statusCode: 400,
-    //         body: JSON.stringify({
-    //             message: 'Bad Request',
-    //             input: event.body
-    //         }),
-    //     };
-    // }
-
     let slsChrome = null;
     let browser = null;
     let page = null;
@@ -42,32 +32,40 @@ module.exports.main = async (event, context, callback) => {
             browserWSEndpoint: (await CDP.Version()).webSocketDebuggerUrl,
         });
         page = await browser.newPage();
-        await page.authenticate({username: parameters['ProxyUserName'], password: parameters['ProxyPassword']});
+        await page.authenticate({ username: parameters['ProxyUserName'], password: parameters['ProxyPassword'] });
 
-        await page.goto(parameters['SignInUrl'], {waitUntil: 'domcontentloaded'});
-
-        let title = await page.evaluate(() => {
-            return document.title;
-        });
-        console.log(title);
+        await page.goto(parameters['SignInUrl'], { waitUntil: 'domcontentloaded' });
+        // await page.waitForNavigation({ timeout: 10000, waitUntil: 'domcontentloaded' });
+        // console.log(await page.title());
 
         await page.type('input[name="user_id"]', parameters['SignInUserName']);
         await page.type('input[name="password"]', parameters['SignInPassword']);
-        let buttonElement = await page.$(
-            'body > form > table:nth-child(9) > tbody > tr > td:nth-child(1) > input[type="button"]'
-        );
-        await buttonElement.click({waitUntil: 'domcontentloaded'});
-        await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
-        // await page.screenshot({path: 'enter.png', fullPage: true});
-        let data = await page.$eval('body > form > table:nth-child(7) > tbody > tr > td > font > b', e => {
-            return e.textContent;
+        await page.click('body > form > table:nth-child(9) > tbody > tr > td:nth-child(2) > input[type="button"]', {
+            waitUntil: 'domcontentloaded',
         });
-        console.log(data);
+        // console.log(await page.title());
+        // await page.waitForNavigation({ timeout: 10000, waitUntil: 'domcontentloaded' });
+        // await page.screenshot({path: 'enter.png', fullPage: true});
+        let selector = 'body > form > table:nth-child(7) > tbody > tr > td > font > b';
+        await page.waitFor(selector, { timeout: 10000 });
+        let message = await page.$eval(selector, e => {
+            return e.textContent.replace(/\t/g, '');
+        });
+        console.log(message);
 
-        return callback(null, JSON.stringify({result: 'Success'}));
+        params = {
+            Message: 'Succeeded entering',
+            Subject: message,
+            TopicArn: 'arn:aws:sns:ap-northeast-1:274682760725:my-topic',
+        };
+        sns.publish(params, (err, data) => {
+            console.log(data);
+        });
+
+        return callback(null, JSON.stringify({ result: 'Success' }));
     } catch (err) {
         console.error(err);
-        return callback(null, JSON.stringify({result: 'Failure'}));
+        return callback(null, JSON.stringify({ result: 'Failure' }));
     } finally {
         if (page) {
             await page.close();
